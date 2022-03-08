3ce0d4c543a40f11f4303a207bb279337b54b98d
Vamsi Krishna <65168347+Vamsi1307@users.noreply.github.com>
2022-03-09 00:21:52 +0530

    Initial Checkin
    Committed the implementation of Azure Cache for Redis in DotNet6 Application

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RedisDotNet6.Models;
+
+namespace RedisDotNet6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
